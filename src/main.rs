d0f4491fb3c0f1524cc941123c81518e790a853b
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2023-02-10 15:34:15 +0100

    Implement a simple tungstenite websocket server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,43 @@
+use std::net::TcpListener;
+use std::thread::spawn;
+use tungstenite::{
+    accept_hdr,
+    handshake::server::{Request, Response},
+};
+use std::env;
+
 
 fn main() {
+    let _ = env_logger::init();
+    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    let server = TcpListener::bind(addr).unwrap();
+    // info!("Listening on: {}", addr);
+    for stream in server.incoming() {
+        spawn(move || {
+            let callback = |req: &Request, mut response: Response| {
+                println!("Received a new ws handshake");
+                println!("The request's path is: {}", req.uri().path());
+                println!("The request's headers are:");
+                for (ref header, _value) in req.headers() {
+                    println!("* {}", header);
+                }
 
+                // Let's add an additional header to our response to the client.
+                let headers = response.headers_mut();
+                headers.append("MyCustomHeader", ":)".parse().unwrap());
+                headers.append("SOME_TUNGSTENITE_HEADER", "header_value".parse().unwrap());
+
+                Ok(response)
+            };
+            let mut websocket = accept_hdr(stream.unwrap(), callback).unwrap();
+            loop {
+                let msg = websocket.read_message().unwrap();
+
+                // We do not want to send back ping/pong messages.
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
+                }
+            }
+        });
+    }
 }
