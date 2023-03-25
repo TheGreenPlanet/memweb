a7d588e20f80bdc41a0c577060b83dea9f5800da
TheGreenPlanet <48806927+TheGreenPlanet@users.noreply.github.com>
2023-03-25 14:27:33 +0100

    Python script send read msg now

--- websocket_client_tester.py
+++ websocket_client_tester.py
@@ -4,7 +4,8 @@ import websockets
 
 async def connect():
     async with websockets.connect("ws://127.0.0.1:8080") as websocket:
-        await websocket.send("Hello, world!")
+        read_msg = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01'
+        await websocket.send(read_msg)
         while True:
             response = await websocket.recv()
             print("Server: ")
